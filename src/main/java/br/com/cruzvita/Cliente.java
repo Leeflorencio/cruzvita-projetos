aeaeca422fb1dfdb4bcd11eafaa9712f2a5b3d13
leticia anjos <florenciolee63@gmail.com>
2023-02-16 18:14:51 -0300

    Atualizações de criação de métodos e atributos nas classes Banco, Cliente e Conta, mas a criação da interface.

--- src/main/java/br/com/cruzvita/Cliente.java
+++ src/main/java/br/com/cruzvita/Cliente.java
@@ -3,7 +3,7 @@ package br.com.cruzvita;
 public class Cliente {
     public static void main(String[] args) {
     }
-    public String nome;
+    String nome;
     int idade;
     String cpf;
     String endereco;
